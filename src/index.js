8fae8baf17cd2f09398967cedaff73aebee73730
Amanda Holanda <amandaholanda_@hotmail.com>
2022-11-22 22:02:16 -0300

    reorganização das rotas na pagina routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from './pages/login/login';
-import Signup from './pages/signup/signup.js';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AllRoutes from './routes/index'
+import { BrowserRouter } from 'react-router-dom';
 import './global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="signup/*" element={<Signup />} />
-    </Routes>
+    <AllRoutes />
   </BrowserRouter>
 );
