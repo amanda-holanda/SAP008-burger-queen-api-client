8bb41736fe476fd45f02742a9614e75d840484f7
Bellalimalima <isabellalimaelima@gmail.com>
2022-12-13 17:06:35 -0300

    loginusuario

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,4 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link,  useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import React from "react";
+import { createUser, setTokenPosition } from "../../services/services";
+
+export const Register = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [positionUser, setPositionUser] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+  
+    const handleChangePosition = (e) => {
+      setPositionUser(e.target.value);
+    }
+    const handleCreateUser = (e) => {
+      e.preventDefault();
+      createUser(name, email, password, positionUser)
+        .then((response) => response.json())
+        .then((obj) => {
+          if (obj.code) {
+            throw (obj.message)
+          } else {
+            return obj
+          }
+        })
+        .then((data) => {
+          if(!data) return;
+          setTokenPosition(data.token, data.position);
+          console.log(data);
+          navigate("/");
+        })
+        .catch((error) => setError(error));
+    }};
 
 function Signup() {
     return (
--- src/services/services.js
+++ src/services/services.js
@@ -1,30 +1,52 @@
-const baseUrl = 'https://lab-api-bq.onrender.com/'; //tá certo?
-const apiCreateUser = `${baseUrl}/users/`;
+export const setTokenPosition = (token, position) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('position', position);
+  }
 
-//criar uma função pra montar esse objeto. tem que receber os dados do input. 
-//tem que ser chamada nos componentes do react
+export const getToken = () => localStorage.getItem('token');
+export const getPosition = () => localStorage.getItem('position');
 
-const createUser = () => {
-    const creatingUsers = {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json',        
-        },
-        body: {
-            "name": "Sample Name - optional", //trocar pelos dados do input,
-            "email": "sample@mail.com",
-            "password": "sample",
-            "role": "sample role",
-            "restaurant": "sample restaurant"
-        }
-    }
-}
+export const createUser = (name, email, password, position) => {
+    return fetch('https://lab-api-bq.onrender.com/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+        position: position,
+        restaurant: "Hamburgreen",
+      }),
+    })};
 
-export const fetchCreateUser = async () => {
-    fetch(apiCreateUser, createUser)
-        .then((response) => response.json())
-        //colocar outro then com data (print ju)
-}
+// // const baseUrl = 'https://lab-api-bq.onrender.com/'; //tá certo?
+// // const apiCreateUser = `${baseUrl}/users/`;
+// //criar uma função pra montar esse objeto. tem que receber os dados do input. 
+// //tem que ser chamada nos componentes do react
+
+// const createUser = () => {
+//     const creatingUsers = {
+//         method: 'POST',
+//         headers: {
+//             'Content-type': 'application/json',        
+//         },
+//         body: {
+//             "name": "Sample Name - optional", //trocar pelos dados do input,
+//             "email": "sample@mail.com",
+//             "password": "sample",
+//             "role": "sample role",
+//             "restaurant": "sample restaurant"
+//         }
+//     }
+// }
+
+// export const fetchCreateUser = async () => {
+//     fetch(apiCreateUser, createUser)
+//         .then((response) => response.json())
+//         //colocar outro then com data (print ju)
+// }
 
 
 //passo a passo
