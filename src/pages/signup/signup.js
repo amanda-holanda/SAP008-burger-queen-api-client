f0a45dcab1a3972cc090eb65e0db3cc96ad606a9
Amanda Holanda <amandaholanda_@hotmail.com>
2022-12-14 18:21:03 -0300

    alinhamento do form

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,72 +1,73 @@
 import Select from '../../components/form/select'
 import Input from '../../components/form/input'
 import SubmitButton from '../../components/form/submitButton'
-import { Link, useNavigate  } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from "react";
 import React from "react";
 import { createUser, setTokenPosition } from "../../services/services";
 import hamburgreenLogo from "../../img/hamburgreen-logo.png";
 
 function Signup() {
-    return (
-        <form>
-            <img src={hamburgreenLogo} alt="hamburgreen logo" className='hamburgreenLogo'/>            
-            <Link to="/">Voltar</Link>
-            <h1 className='cadastro'>Cadastro</h1>
-            <Input
-                type="text"
-                id="name"
-                placeholder="Nome"
-                name="name"
-            />
-            <Input
-                type="email"
-                id="email"
-                placeholder="Email"
-                name="email"
-            />
-            <Input
-                type="password"
-                id="password"
-                placeholder="Senha"
-                name="password"
-            />
-            <Select name="cargo" text="Cargo" />
-            <SubmitButton type="submit" text="Cadastrar" />
+  return (
+    <form className='formSignup'>
+      <img src={hamburgreenLogo} alt="hamburgreen logo" className='hamburgreenLogo' />
+      <Link to="/">Voltar</Link>
+      <h1 className='cadastro'>Cadastro</h1>
+      <Input
+        type="text"
+        id="name"
+        placeholder="Nome"
+        name="name"
+      />
+      <Input
+        type="email"
+        id="email"
+        placeholder="Email"
+        name="email"
+      />
+      <Input
+        type="password"
+        id="password"
+        placeholder="Senha"
+        name="password"
+      />
+      <Select name="cargo" text="Cargo" />
+      <SubmitButton type="submit" text="Cadastrar" />
 
-        </form>
-    )
+    </form>
+  )
 }
 
 export const Register = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [positionUser, setPositionUser] = useState("");
-    const [error, setError] = useState("");
-    const navigate = useNavigate();
-  
-    const handleChangePosition = (e) => {
-      setPositionUser(e.target.value);
-    }
-    const handleCreateUser = (e) => {
-      e.preventDefault();
-      createUser(name, email, password, positionUser)
-        .then((response) => response.json())
-        .then((obj) => {
-          if (obj.code) {
-            throw (obj.message)
-          } else {
-            return obj
-          }
-        })
-        .then((data) => {
-          if(!data) return;
-          setTokenPosition(data.token, data.position);
-          console.log(data);
-          navigate("/");
-        })
-        .catch((error) => setError(error));
-    }};
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [positionUser, setPositionUser] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleChangePosition = (e) => {
+    setPositionUser(e.target.value);
+  }
+  const handleCreateUser = (e) => {
+    e.preventDefault();
+    createUser(name, email, password, positionUser)
+      .then((response) => response.json())
+      .then((obj) => {
+        if (obj.code) {
+          throw (obj.message)
+        } else {
+          return obj
+        }
+      })
+      .then((data) => {
+        if (!data) return;
+        setTokenPosition(data.token, data.position);
+        console.log(data);
+        navigate("/");
+      })
+      .catch((error) => setError(error));
+  }
+};
 
 export default Signup;
