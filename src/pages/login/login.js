0549a2b65090f7bf828270a57f007b0ed0b77095
Gleyci-Macena <gleycianemc@gmail.com>
2022-12-15 13:51:48 -0300

    feature css login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -2,27 +2,31 @@ import Select from '../../components/form/select'
 import Input from '../../components/form/input'
 import SubmitButton from '../../components/form/submitButton'
 import { Link } from 'react-router-dom';
+import hamburgreenLogo from '../../img/hamburgreen-logo.png';
 
 function Login() {
     return (
-        <form>
-            <h1>Login</h1>
+        <form className='login'>
+            <img className='imgLogin' alt="Logo Hamburgreen" src={hamburgreenLogo} />
+            <h1 className='tittle'>Login</h1>
             <Input
                 type="email"
                 id="email"
-                placeholder="Email"
+                placeholder=" E-mail"
                 name="email"
             />
             <Input
                 type="password"
                 id="password"
-                placeholder="Senha"
+                placeholder=" Senha"
                 name="password"
             />
+            <div className='loginbtn'>
             <Select name="cargo" text="Cargo" />
             <SubmitButton type="submit" text="Entrar" />
-            <span>Não possui cadastro?</span>
-            <Link to="signup/">Cadastre-se</Link>
+            </div>
+            <span className='noSignUp'>Não possui cadastro? <Link className='signUp' to="signup/"> Cadastre-se</Link></span>
+            
         </form >
     )
 }
