cba868c9d012ec1e57ee0de8e2bc8e141f9939ab
Bellalimalima <isabellalimaelima@gmail.com>
2022-12-15 12:58:12 -0300

    autenticação

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,10 +1,40 @@
 import Select from '../../components/form/select'
 import Input from '../../components/form/input'
 import SubmitButton from '../../components/form/submitButton'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { login, setTokenRole } from "../../services/services";
 
-function Login() {
-    return (
+export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+  
+    const handleLogin = (e) => {
+      e.preventDefault();
+      login(email, password)
+        .then((response) => response.json())
+        .then((obj) => {
+          if (obj.code) {
+            throw (obj.message);
+          } else {
+            return obj;
+          }
+        })
+        .then((data) => {
+          if (!data) return;
+          setTokenRole(data.token, data.position);
+          if (data.role === 'garçom') {
+            navigate('/menu');
+          } if (data.role === 'chefe') {
+            navigate('/cozinha');
+          } 
+        })
+        .catch((error) => setError(error));
+    }
+
+  return (
         <form>
             <h1>Login</h1>
             <Input
@@ -12,19 +42,21 @@ function Login() {
                 id="email"
                 placeholder="Email"
                 name="email"
+                onChange={(e) => setEmail(e.target.value)}
             />
             <Input
                 type="password"
                 id="password"
                 placeholder="Senha"
                 name="password"
+                onChange={(e) => setPassword(e.target.value)}
             />
-            <Select name="cargo" text="Cargo" />
-            <SubmitButton type="submit" text="Entrar" />
+            <p className="errorMsg">{error}</p>
+            <Select name="cargo" text="Cargo"/>
+            <SubmitButton onClick={handleLogin} type="submit" text="Entrar" />
             <span>Não possui cadastro?</span>
             <Link to="signup/">Cadastre-se</Link>
         </form >
     )
-}
+};
 
-export default Login;
--- src/services/services.js
+++ src/services/services.js
@@ -1,50 +1,43 @@
-export const setTokenPosition = (token, position) => {
+export const setTokenRole = (token, role) => {
     localStorage.setItem('token', token);
-    localStorage.setItem('position', position);
+    localStorage.setItem('role', role);
   }
 
 export const getToken = () => localStorage.getItem('token');
-export const getPosition = () => localStorage.getItem('position');
+export const getRole = () => localStorage.getItem('role');
 
-export const createUser = (name, email, password, position) => {
+export const createUser = (name, email, password, role) => {
     return fetch('https://lab-api-bq.onrender.com/users', {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        name: name,
-        email: email,
-        password: password,
-        position: position,
+        name,
+        email,
+        password,
+        role,
         restaurant: "Hamburgreen",
       }),
     })};
 
-// // const baseUrl = 'https://lab-api-bq.onrender.com/'; //tá certo?
-// // const apiCreateUser = `${baseUrl}/users/`;
-// //criar uma função pra montar esse objeto. tem que receber os dados do input. 
-// //tem que ser chamada nos componentes do react
 
-// const createUser = () => {
-//     const creatingUsers = {
-//         method: 'POST',
-//         headers: {
-//             'Content-type': 'application/json',        
-//         },
-//         body: {
-//             "name": "Sample Name - optional", //trocar pelos dados do input,
-//             "email": "sample@mail.com",
-//             "password": "sample",
-//             "role": "sample role",
-//             "restaurant": "sample restaurant"
-//         }
-//     }
-// }
-
-// export const fetchCreateUser = async () => {
-//     fetch(apiCreateUser, createUser)
-//         .then((response) => response.json())
-//         //colocar outro then com data (print ju)
-// }
-
+export const login = (email, password) => fetch('https://lab-api-bq.onrender.com/auth', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    
+export const accessUser = () => fetch('https://lab-api-bq.onrender.com/users', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: getToken(),
+      },
+    });
+    
