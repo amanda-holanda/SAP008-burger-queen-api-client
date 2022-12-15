79a916605be29c940a45bf7692917a7915c0c78f
Amanda Holanda <amandaholanda_@hotmail.com>
2022-12-15 12:57:50 -0300

    estilização do select

--- src/components/form/select.js
+++ src/components/form/select.js
@@ -1,12 +1,8 @@
 function Select({ text, name, options, handleOnChange, value }) {
     return (
-        <div>
-            <select name={name} id={name}>
-                <option disabled selected>Cargo</option>
-                
-            </select>
-        </div>
-
+        <select className="selectSignup" name={name} id={name}>
+            <option disabled selected>Cargo</option>
+        </select>
     )
 }
 
